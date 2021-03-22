77b319b2694ff68029a5ab7282b12e8496b5bdba
Krati Ahuja <kratiahuja@gmail.com>
2021-03-21 17:31:17 -0700

    Basic routing

---
+++ src/MyContainer.js
@@ -0,0 +1,79 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    padding: 10,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: "center",
+  }
+}));
+
+export default function CenteredGrid() {
+  console.log(useStyles);
+  const classes = useStyles();
+
+  return (
+    <Router>
+      <div className={classes.root}>
+        <Grid container spacing={3}>
+          <Grid item xs={2}>
+            <Paper className={classes.paper}>
+              <nav>
+                <ul>
+                  <li>
+                    <Link to="/">Home</Link>
+                  </li>
+                  <li>
+                    <Link to="/about">About</Link>
+                  </li>
+                  <li>
+                    <Link to="/users">Users</Link>
+                  </li>
+                </ul>
+              </nav>
+            </Paper>
+          </Grid>
+          <Grid item xs={10}>
+            <Paper className={classes.paper}>
+              <Switch>
+                <Route path="/about">
+                  <About />
+                </Route>
+                <Route path="/users">
+                  <Users />
+                </Route>
+                <Route path="/">
+                  <Home />
+                </Route>
+              </Switch>
+            </Paper>
+          </Grid>
+        </Grid>
+      </div>
+    </Router>
+  );
+}
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
